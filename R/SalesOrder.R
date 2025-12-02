1e9f443c6f13eeb0bb2f419a9b7c7271ab57458a
takewiki <hulilei@takeiwki.com.cn>
2025-12-02 09:59:34 +0000

    v1.0.0

---
+++ R/SalesOrder.R
@@ -0,0 +1,65 @@
+
+#' 处理逻辑
+#'
+#' @param input 输入
+#' @param output 输出
+#' @param session 会话
+#' @param dms_token 口令
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' SalesOrderSelectServer()
+SalesOrderSelectServer <- function(input,output,session,dms_token) {
+  #获取参数
+  text_SalesOrder=tsui::var_text('text_SalesOrder')
+
+
+  #查询按钮
+
+  shiny::observeEvent(input$btn_SalesOrder_view,{
+
+    FSalesOrder=text_SalesOrder()
+
+
+    if(FSalesOrder==''){
+
+      tsui::pop_notice("Please Enter Sales Order")
+
+
+    }else{
+      data = mdlVmSalesOrderr::SalesOrder_selectByDate(dms_token = dms_token,FCalculateYear = FCalculateYear,FLatestVersion =FLatestVersion ,FVersion = FVersion,FCalculationPeriod = FCalculationPeriod)
+
+      tsui::run_dataTable2(id ='SalesOrder_resultView' ,data =data )
+
+      tsui::run_download_xlsx(id = 'dl_SalesOrder',data = data,filename = 'SalesOrder.xlsx')
+
+
+    }
+
+
+  })
+
+
+
+}
+
+
+#' 处理逻辑
+#'
+#' @param input 输入
+#' @param output 输出
+#' @param session 会话
+#' @param dms_token 口令
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' SalesOrderServer()
+SalesOrderServer <- function(input,output,session,dms_token) {
+  SalesOrderSelectServer(input = input,output = output,session = session,dms_token = dms_token)
+
+
+}
